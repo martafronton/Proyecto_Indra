b1b72cd8bd0182ee672ead7cf1f9ccfc53a323be
martafronton <martafronton@gmail.com>
2025-02-27 21:30:33 +0100

    Cambios programa
    Añado métodos

--- Evento.kt
+++ Evento.kt
@@ -1,23 +1,50 @@
 class Evento {
     var nombre=""
     var fecha=0
+    var duracion=0
     var ubicacion=Ubicacion()
     var categoria=Categoria.CHARLA
     var organizador=Organizador()
+    var nummaximo=100
     var fichaUsuarios=ArrayList<Usuario>()
 
     constructor()
-    constructor(nombre: String, fecha: Int, ubicacion: Ubicacion, categoria:Categoria, organizador: Organizador) {
+    constructor(nombre: String, fecha: Int, ubicacion: Ubicacion, categoria:Categoria, organizador: Organizador, nummaximo:Int) {
         this.nombre = nombre
         this.fecha = fecha
         this.ubicacion = ubicacion
         this.categoria = categoria
         this.organizador = organizador
+        this.nummaximo=nummaximo
+        totalEventos++
+
+    }
+    companion object{
+        var totalEventos=0
+    }
+    fun inscribir(usuario:Usuario,fecha:Int):Inscripcion{
+        var inscripcion=Inscripcion()
+        var inscribir=true
+        if(fichaUsuarios.size>nummaximo){
+            inscribir=false
+        }
+        for(i in fichaUsuarios.indices) {
+            if (fichaUsuarios[i] == usuario) {
+                inscribir = false
+            }
+        }
+        if(inscribir) {
+            fichaUsuarios.add(usuario)
+            usuario.listaEventos.add(this)
+            inscripcion = Inscripcion(usuario, this, fecha)
+        }
+        return inscripcion
+    }
+    fun contarUsuariosInscritos(): Int {
+        return fichaUsuarios.size
+    }
+   override fun toString(): String {
+        return "Evento: $nombre Fecha: $fecha Ubicación: ${ubicacion.direccion} Categoría: ${categoria.name} Organizador: ${organizador.nombre}"
     }
 
-    fun inscribir(usuario:Usuario,fecha:Int){
-        fichaUsuarios.add(usuario)
-        usuario.listaEventos.add(this)
-        var inscripcion=Inscripcion(usuario,this, fecha)
-    }
 }
--- Ubicacion.kt
+++ Ubicacion.kt
@@ -1,11 +1,20 @@
 class Ubicacion {
     var online=false
-    var direccion=""
+    var direccion="" //tanto si es una dirección física o un enlace, se guardará aquí
 
     constructor()
     constructor(online: Boolean, direccion: String) {
         this.online = online
         this.direccion = direccion
+    }
+    override fun toString(): String {
+        var lugar=""
+        if (online) {
+            lugar="Evento online, enlace: $direccion"
+        } else {
+            lugar="Dirección: $direccion"
+        }
+        return lugar
     }
 
 }
